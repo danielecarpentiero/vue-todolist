aec53a9a24de64c712871ecddac8c3797c44a504
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-28 17:51:40 +0100

    feat milestone 2

--- main.mjs
+++ main.mjs
@@ -2,6 +2,18 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      tasks: [
+        { text: "Go to the movie", done: true },
+        { text: "Physical exercise", done: false },
+        { text: "Playing videogames", done: true },
+        { text: "Read a book", done: true },
+      ],
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
   },
 }).mount("#app");
